379c6cb81559b08f8277abab87000cd4668b0a08
Azmi Haider <azmi.haider92@gmail.com>
2023-04-23 16:55:01 +0300

    batch sampler added, and saving checkpoints

--- train.py
+++ train.py
@@ -7,6 +7,21 @@ import sys
 from tqdm import tqdm
 
 
+class SimpleSampler:
+    def __init__(self, total, batch):
+        self.total = total
+        self.batch = batch
+        self.curr = total
+        self.ids = None
+
+    def nextids(self):
+        self.curr+=self.batch
+        if self.curr + self.batch > self.total:
+            self.ids = torch.LongTensor(np.random.permutation(self.total))
+            self.curr = 0
+        return self.ids[self.curr:self.curr+self.batch]
+
+
 if __name__ == '__main__':
     torch.set_default_dtype(torch.float32)
     torch.manual_seed(20211202)
@@ -17,14 +32,24 @@ if __name__ == '__main__':
     training_images = load_data_arrays(args.images_dir)  # images are normalized from 0 to 1
     diffusion = create_diffusion_model(args.images_dim, args.images_channel)
     pbar = tqdm(range(args.num_epochs), miniters=1, file=sys.stdout)
+
+    trainingSampler = SimpleSampler(training_images.shape[0], args.batch_size)
     for epoch in pbar:
-        loss = diffusion(training_images)
+        # batch
+        ids = trainingSampler.nextids()
+        batch = training_images[ids]
+
+        # model update
+        loss = diffusion(batch)
         loss.backward()
 
+        # result
         l = loss.detach().item()
         pbar.set_description(
             f'Iteration {epoch:05d}:'
             + f' mse = {l:.6f}')
 
-    sampled_planes = diffusion.sample(batch_size=1)
+    # save model
+    torch.save(diffusion.state_dict(), './checkpoints/diffusion.pt')
+    #sampled_planes = diffusion.sample(batch_size=1)
 
